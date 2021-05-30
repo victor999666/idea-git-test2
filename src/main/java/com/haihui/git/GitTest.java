4ddd6c94c0af4513bfe2b003ebed96eaf539146a
victor999666 <79508749+victor999666@users.noreply.github.com>
2021-05-30 13:17:00 +0800

    pull commit ==> update - GitTest.java

--- src/main/java/com/haihui/git/GitTest.java
+++ src/main/java/com/haihui/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("idea-git-victor1");
         System.out.println("idea-git-victor2");
+        System.out.println("pull-git");
     }
 }
