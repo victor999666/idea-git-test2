e12521dce85a3f55f3ce138ab48371c229d3b170
victor999666 <2719116199@qq.com>
2021-05-30 12:51:39 +0800

    second commit2

--- src/main/java/com/haihui/git/GitTest.java
+++ src/main/java/com/haihui/git/GitTest.java
@@ -6,5 +6,6 @@ package com.haihui.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("idea-git-victor1");
+        System.out.println("idea-git-victor2");
     }
 }
